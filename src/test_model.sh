fe5ab3cda0fa873742770bcf35a7dbb53ec3eb93
dt-lindberg <dt.lindberg@gmail.com>
2025-05-20 16:48:57 +0200

    Added sh file for model testing

---
+++ src/test_model.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=18
+#SBATCH --gpus=1
+#SBATCH --partition=gpu_a100
+#SBATCH --time=01:00:00
+#SBATCH --job-name=ModelTest
+#SBATCH --output=slurm_output_%A.out
+
+# Loading modules (CUDA and Anaconda are located in module 2024)
+module load 2024
+module load CUDA/12.6.0
+module load Anaconda3/2024.06-1
+
+# Activate conda environment
+source activate lips_env
+
+# Logging info
+echo "Starting job at $(date)"
+
+# Run the python script
+srun python $HOME/arc25/src/load_model.py
+
+echo "Finished job at $(date)"
